1841f280c70274441a19911552503be30547fc29
fanfr <1979463718@qq.com>
2023-09-19 16:49:39 +0800

    Changes

--- machinelearning/Logistic_regression.py
+++ machinelearning/Logistic_regression.py
@@ -1,5 +1,5 @@
 from __future__ import print_function
-from numpy import *
+import numpy as np
 import matplotlib.pyplot as plt
 
 ## 使用logistic regression 实现数据及分类，修改正则化
@@ -10,20 +10,51 @@ def load_data(file_name):
     for line in fr.readlines():
         dataline=[]
         lineArr = line.strip().split()
-        for x in range(0,len(lineArr)-2):
+        for x in range(0,len(lineArr)-1):
             dataline.append(float(lineArr[x]))
+        dataline.append(1.0)
         dataMat.append(dataline)
-        labelMat.append(lineArr[-1])
-    print(dataMat)
-    print(labelMat)
+        labelMat.append(int(float(lineArr[-1])))
+    # print(dataMat)
+    # print(labelMat)
+    dataMat = np.mat(dataMat)
+    print("the size of x matrix: " + str(dataMat.shape))
+    labelMat = np.mat(labelMat)
+    print("the size of label matrix: " + str(labelMat.shape))
     return dataMat,labelMat
-# print(load_data("data/5.Logistic/TestSet.txt"))
 
 
 def sigmoid(X):
-    return 1/(1+exp(-X))
+    return 1/(1+np.exp(-X))
+
+def loss_functin(dataMat,labelMat,weight):
+    J=np.dot(labelMat,np.dot(dataMat,weight))-np.log(1+np.dot(dataMat,weight))
+    # J=np.dot(labelMat,np.dot(dataMat,weight))-sigmoid(np.dot(dataMat,weight))*dataMat
+    loss_value=-np.average(J)
+    return loss_value
+def get_weights_update(dataMat,labelMat,weight):
+    gradient_update=J=np.dot(labelMat,dataMat)-sigmoid(np.dot(dataMat,weight))*dataMat
 
 
 
 def gradAscent(dataMat,labelMat):
+    ###设置算法停止次数
+    terminate_condition_T=400
+    ###设置学习率
+    learning_rate=0.01
+    ###设置初始参数值 全为1
+    weights=np.ones(22)
+
+    t=0
+    while t<terminate_condition_T:
+        weights=weights+
+
+
+
+
+
     return
+
+
+datamat,labelMat=load_data("data/5.Logistic/HorseColicTraining.txt")
+gradAscent(datamat,labelMat)
